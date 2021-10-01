7e4d9d08459e26a88d9b59980310909acf949bbc
jdbarajass <jdbarajass@gmail.com>
2021-10-01 11:55:25 -0500

    agregar boton de login y modificacion del navbar

--- src/componentes/Sidebar.jsx
+++ src/componentes/Sidebar.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 const Sidebar = () => {
     return (
-        <div>
+        <nav className = "w-72 bg-red-400">
             Sidebar 
-        </div>
+        </nav>
     )
 }
 
-export default Sidebar 
+export default Sidebar
+// el codigo w-72 significa w = ancho y w-72 es 18rem es decir 288 pixeles es la mitad de una pantalla mediana
+// el codigo bg-red-400 es el color de fondo que le damos al sidebar y el 400 es el color que uno haya escogido en este caso rojo que puede variar su color rojo entre 100 y 900
--- src/layouts/PrivateLayout.jsx
+++ src/layouts/PrivateLayout.jsx
@@ -1,13 +1,20 @@
-import Sidebar from 'componentes/Sidebar'   
-import React from 'react'
+import Sidebar from "componentes/Sidebar";
+import React from "react";
 
-const PrivateLayout = ({children}) => {
-    return (
-        <div>
-            <Sidebar />
-            { children}
-        </div>
-    )
-}
+const PrivateLayout = ({ children }) => {
+  return (
+    <div className="flex w-screen h-screen">
+      <Sidebar />
+          <main className = "flex w-full bg-blue-400 overflow-y-scroll">
+              {children}
+          </main>
+    </div>
+  );
+};
 
 export default PrivateLayout;
+// Display flex o flex = Que pueda ocupar todo el espacio en la pantalla
+// w-screen h-screen = Este codigo dice que ocupe todo el alto y el ancho del view port
+// <main className="flex w-full bg-blue-400">{children}</main>; = este codigo es para que  /// todo el contenido de la pagina ocupe toda la pantalla y ademas tenga ese color azul
+// Y esta plantilla del PribeteLayout lo que hace es que tenga mi sidebar a un lado y mi    //contenido al otro lado
+// overflow-y-scroll =Este codigo sirve para que el sidebar se quede quieto y mi contenido  //sea el que se mueva de lado a lado   
